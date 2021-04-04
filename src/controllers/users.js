fce2a62caa330bc68102019330c35402e9407fd9
Manuel Vera <manuelvera202@gmail.com>
2021-04-04 13:09:22 -0300

    DB validation

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const User = require("../models/user");
+const userService = require("../services/userService");
+const Success = require("../handlers/successHandler");
 
 /**
  *
@@ -8,8 +9,12 @@ const User = require("../models/user");
  */
 const getAllUsers = async (req, res, next) => {
   try {
-    const users = await User.find();
-    res.json(users);
+    const users = await userService.findAll(
+      req.query.filter,
+      req.query.options
+    );
+
+    res.json(new Success(users));
   } catch (err) {
     next(err);
   }
@@ -23,13 +28,9 @@ const getAllUsers = async (req, res, next) => {
 const createUser = async (req, res, next) => {
   try {
     let user = req.body;
-    user = await User.create(user);
+    user = await userService.save(user);
 
-    const result = {
-      message: "User created",
-      user,
-    };
-    res.status(201).json(result);
+    res.status(201).json(new Success(user));
   } catch (err) {
     next(err);
   }
@@ -44,15 +45,10 @@ const updateUser = async (req, res, next) => {
   try {
     const { id } = req.params;
     let user = req.body;
-    user._id = id;
 
-    await User.updateOne(user);
+    const userUpdate = await userService.update(id, user);
 
-    const result = {
-      message: "User updated",
-      user,
-    };
-    res.json(result);
+    res.json(new Success(userUpdate));
   } catch (err) {
     next(err);
   }
@@ -63,11 +59,16 @@ const updateUser = async (req, res, next) => {
  * @param {express.Request} req
  * @param {express.Response} res
  */
-const updatePartialUser = (req, res) => {
-  const result = {
-    message: "User updated with patch",
-  };
-  res.json(result);
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const user = await userService.findById(id);
+
+    res.json(new Success(user));
+  } catch (err) {
+    next(err);
+  }
 };
 
 /**
@@ -78,14 +79,10 @@ const updatePartialUser = (req, res) => {
 const deleteUser = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const user = await User.findById(id);
-    user.remove();
 
-    //const id = req.params.id;
-    const result = {
-      message: `User with id: ${id} deleted`,
-    };
-    res.json(result);
+    const user = await userService.remove(id);
+
+    res.json(new Success(user));
   } catch (err) {
     next(err);
   }
@@ -95,6 +92,6 @@ module.exports = {
   getAllUsers,
   createUser,
   updateUser,
-  updatePartialUser,
+  getById,
   deleteUser,
 };
--- src/loaders/mongoose/index.js
+++ src/loaders/mongoose/index.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 const config = require("../../config");
 
 module.exports = async () => {
-  await mongoose.connect(config.databaseURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  });
+  try {
+    await mongoose.connect(config.databaseURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+  } catch (error) {
+    throw new Error();
+  }
 };
